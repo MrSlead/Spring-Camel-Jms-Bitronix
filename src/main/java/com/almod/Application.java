077f2baf7ac856b14831aa15548166848033af82
Mordyasov Alex <mordyasov.60@mail.ru>
2020-11-19 13:36:42 +0700

    added h2repo for handling messages

--- src/main/java/com/almod/Application.java
+++ src/main/java/com/almod/Application.java
@@ -2,7 +2,6 @@ package com.almod;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 
 public class Application {
---
+++ src/main/java/com/almod/db/H2Repo.java
@@ -0,0 +1,64 @@
+package com.almod.db;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Repository
+public class H2Repo {
+    private final Logger LOG = Logger.getRootLogger();
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public H2Repo(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Transactional
+    //@Handler
+    public void writeInDB(InputStream inputStream) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        String sqlMessageDB = "INSERT INTO message(text) VALUES(?)";
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sqlMessageDB, Statement.RETURN_GENERATED_KEYS);
+            try {
+                String text = IOUtils.toString(inputStream, "UTF-8");
+                ps.setString(1, text);
+            } catch (Exception e) {
+                LOG.error(e.getMessage());
+            }
+            return ps;
+        }, keyHolder);
+
+        String sqlHeadersDB = "INSERT INTO headers(id, head) VALUES(?, ?)";
+        try {
+            jdbcTemplate.update(sqlHeadersDB, keyHolder.getKey().longValue(), "Type");
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+    }
+
+    public void showDataDB() {
+        LOG.info("Show result in database");
+        String sql = "SELECT mg.text, hs.head" +
+                " FROM message mg" +
+                " INNER JOIN headers hs" +
+                " ON mg.id = hs.id;";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
+        while(rowSet.next()) {
+            LOG.info(rowSet.getString(1) + "  " + rowSet.getString(2));
+        }
+    }
+}
